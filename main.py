ea1830e0771acb9696e190fd5a6c462c0a2fecc3
naldeull <ljmyoyo@g.skku.edu>
2024-04-11 10:08:02 +0000

    feat: main.py - third version

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# second version
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    # YOUR ANSWER
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i,j]
+    return
